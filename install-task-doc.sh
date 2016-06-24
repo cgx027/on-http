1d2352378194d82d0a73b839cd46e857ef98831e
Ted Chen <ted.chen@emc.com>
2016-06-24 15:38:18 +0000

    Generate task-doc from task schema using 'npm install'
    Add a /task-doc link to host the task-doc html

---
+++ install-task-doc.sh
@@ -0,0 +1,8 @@
+# Copyright 2015, EMC, Inc.
+
+cd ./static
+
+rm -rf task-doc
+# Place holder - generate html files from task schema
+# Place holder - copy generated html to ./static folder
+# place holder - remove generated html files.
