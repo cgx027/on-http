ff1f4eb9b6089a7fcebec970754e2bf8bd1590dd
Ted Chen <ted.chen@emc.com>
2016-07-05 15:27:45 +0000

    Version update

--- install-task-doc.sh
+++ install-task-doc.sh
@@ -2,7 +2,20 @@
 
 cd ./static
 
-rm -rf task-doc
-# Place holder - generate html files from task schema
-# Place holder - copy generated html to ./static folder
-# place holder - remove generated html files.
+# Copy templates from apidoc, taskdoc leverage apidoc templates for html rendering
+cp -rf ../node_modules/apidoc/template/* taskdoc/
+
+# Generate task doc from task schemas
+cd ../node_modules/on-tasks/
+#_mocha 'spec/lib/utils/task-option-doc-generator.js' --require spec/helper
+
+#echo 'define({"api":' > api_data.js
+#cat task_doc_data.json | python -m json.tool >> api_data.js
+#echo '});' >> api_data.js
+
+# Copy generated json file to ./static folder, override existing files
+#cp -f api_data.js ../../static/
+#cp -f task_doc_data.json ../../static/api_data.json
+
+# Remove generated html files.
+#rm api_data.js task_doc_data.json
---
+++ static/taskdoc/api_project.js
@@ -0,0 +1,23 @@
+define({
+  "description": "RackHD API Documentation",
+  "footer": {
+    "title": "Other Information",
+    "content": "<p>&copy;2015, EMC, Inc.</p>\n"
+  },
+  "header": {
+    "title": "About",
+    "content": "<p>For more information on how to use these APIs, please also see our development\nguide documentation at <a href=\"http://rackhd.readthedocs.org/en/latest/development_guide.html\">http://rackhd.readthedocs.org/en/latest/development_guide.html</a></p>\n<p>&copy;2015, EMC, Inc.</p>\n"
+  },
+  "name": "RackHD API Documentation",
+  "title": "RackHD API Documentation",
+  "url": "http://servername:8080",
+  "version": "1.1.0",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-05-24T03:38:20.060Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.3"
+  }
+});
